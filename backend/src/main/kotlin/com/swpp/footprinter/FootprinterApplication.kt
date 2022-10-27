ce5bebc2925e8c999265775282c714852b5783c0
shinhong_park <shp7724@naver.com>
2022-10-27 14:43:57 +0900

    🧱 Initialize Spring Boot project (#3)
    * initialize spring boot project
    
    * configure application.yaml
    
    기존 와플 프로젝트 참고해서 생성

---
+++ backend/src/main/kotlin/com/swpp/footprinter/FootprinterApplication.kt
@@ -0,0 +1,11 @@
+package com.swpp.footprinter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FootprinterApplication
+
+fun main(args: Array<String>) {
+	runApplication<FootprinterApplication>(*args)
+}
