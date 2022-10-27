fc29c0cbf2ee55945f015e5ead2e1ebcbf56d622
Park Shin Hong <shp7724@naver.com>
2022-10-27 20:59:52 +0900

    change tab to spaces

--- backend/src/test/kotlin/com/swpp/footprinter/FootprinterApplicationTests.kt
+++ backend/src/test/kotlin/com/swpp/footprinter/FootprinterApplicationTests.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class FootprinterApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 }
