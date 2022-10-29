05ac282e5c793d994bb4d96b87018bf9e1b3ac16
shinhong_park <shp7724@naver.com>
2022-10-29 23:08:17 +0900

    🧑‍💻 Add GitHub Actions, lint, test, etc. (#5)
    * add unit testing with workflows
    
    * add yarn.lock, fix lint issues
    
    change working directory
    
    add cache-dependency-path
    
    add working directory
    
    add yarn setup
    
    install deps
    
    separate frontend workflows
    
    change path
    
    default working directory
    
    * 🧑‍💻 Add Backend GitHub Actions, lint. (#6)
    
    * add ktlint
    
    * add backend workflows
    
    * fix lint issues
    
    * change lint setting
    
    * bump ktlint version to 11
    
    * build without test
    
    * add sonar cloud analysis
    
    Co-authored-by: shinhong_park <shp7724@naver.com>
    
    * separate lint & test
    
    * change tab to spaces
    
    * create testdb using H2
    
    * use actions@v3
    
    * 👷 Update CI build system (#7)
    
    * add jacoco plugin for coverage
    
    * add test coverage check
    
    * add sonar cloud properties
    
    * add eslint report
    
    * fix workflow issue
    
    * fix file path issue
    
    * trigger sonarcloud workflow for debugging
    
    * use yarn build for type checking
    
    * use gradle cache
    
    * use custom actions
    
    * fix file path issue
    
    * remove debugging code
    
    Co-authored-by: 이현성 <40167175+lees741@users.noreply.github.com>

--- backend/src/main/kotlin/com/swpp/footprinter/FootprinterApplication.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/FootprinterApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class FootprinterApplication
 
 fun main(args: Array<String>) {
-	runApplication<FootprinterApplication>(*args)
+    runApplication<FootprinterApplication>(*args)
 }
--- backend/src/test/kotlin/com/swpp/footprinter/FootprinterApplicationTests.kt
+++ backend/src/test/kotlin/com/swpp/footprinter/FootprinterApplicationTests.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class FootprinterApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
