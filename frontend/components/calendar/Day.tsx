326d9aabe3a75662256a9f87bc6afd746746d7b0
Park Shin Hong <shp7724@naver.com>
2022-10-30 12:11:48 +0900

    add icons & demo uis

--- frontend/components/calendar/Day.tsx
+++ frontend/components/calendar/Day.tsx
@@ -5,10 +5,12 @@ interface IProps {
 
 const DayButton = ({ weekday, day }: IProps) => {
   return (
-    <div className="text-center">
-      <div className="text-sm">{weekday}</div>
-      <div className="mt-1 rounded-full py-1 hover:cursor-pointer hover:bg-navy-800">
-        {day}
+    <div className="flex justify-center text-center">
+      <div>
+        <div className="text-sm text-navy-200">{weekday}</div>
+        <div className="mt-1 flex h-10 w-10 items-center justify-center rounded-full hover:cursor-pointer hover:bg-navy-800">
+          {day}
+        </div>
       </div>
     </div>
   );
--- frontend/pages/footprints/index.tsx
+++ frontend/pages/footprints/index.tsx
@@ -1,9 +1,11 @@
 import Calendar from "../../components/calendar/Calendar";
 import Container from "../../components/containers/Container";
+import NavigationBar from "../../components/navbar/NavigationBar";
 
 export default function Footprints() {
   return (
     <Container>
+      <NavigationBar />
       <Calendar />
     </Container>
   );
