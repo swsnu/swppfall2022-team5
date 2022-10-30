732f2d95458a54a4430e99b1f893f2d568a1c633
JuTaK97 <didwngus01@snu.ac.kr>
2022-10-30 13:43:36 +0900

    check style

--- backend/src/main/kotlin/com/swpp/footprinter/core/common/model/BaseEntity.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/core/common/model/BaseEntity.kt
@@ -2,7 +2,11 @@ package com.swpp.footprinter.core.common.model
 
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
-import javax.persistence.*
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 open class BaseEntity(
