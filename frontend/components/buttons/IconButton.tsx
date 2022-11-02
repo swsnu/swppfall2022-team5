19a74386664978c44c3f0dc7a8a67295c944f4e3
shinhong_park <shp7724@naver.com>
2022-11-02 22:09:40 +0900

    ✨ Add frontend skeleton UI using tailwindcss (#8)
    * initialize tailwindcss
    
    * add moment
    
    * customize theme
    
    * add sample pages
    
    * add icons & demo uis

---
+++ frontend/components/buttons/IconButton.tsx
@@ -0,0 +1,19 @@
+import { TablerIcon } from "@tabler/icons";
+
+interface IProps {
+  icon: TablerIcon;
+  onClick: () => void;
+}
+
+const IconButton = ({ icon: Icon, onClick }: IProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className="rounded-full p-2 hover:cursor-pointer hover:bg-navy-800"
+    >
+      <Icon />
+    </div>
+  );
+};
+
+export default IconButton;
