8f7dc6048075ed5d2b026f9324c709de8e3fa0d8
huGgW <whjoon0225@naver.com>
2022-11-03 14:42:20 +0900

    Changed testing packages and configs

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }],
+    "presets": [['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-typescript',
         "next/babel",
     ],
+    "plugins": [
+        "@babel/proposal-class-properties",
+        "@babel/proposal-object-rest-spread",
+    ]
 };
