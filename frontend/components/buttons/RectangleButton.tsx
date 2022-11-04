3b7c0d31aedb279c494aee28664b080caedad5da
Park Shin Hong <shp7724@naver.com>
2022-11-04 11:32:44 +0900

    change button design

--- frontend/components/buttons/RectangleButton.tsx
+++ frontend/components/buttons/RectangleButton.tsx
@@ -9,17 +9,15 @@ interface IProps {
 
 const RectangleButton = ({ onClick, text, isLoading, className }: IProps) => {
   return (
-    <div className="grid justify-items-stretch px-3">
-      <button
-        className={classNames(
-          className,
-          "rounded-lg border border-navy-200/5 bg-navy-700 p-3 text-navy-200 transition-colors hover:bg-navy-600",
-        )}
-        onClick={onClick}
-      >
-        {text}
-      </button>
-    </div>
+    <button
+      className={classNames(
+        className,
+        "rounded-lg border border-navy-200/5 bg-navy-700 p-3 text-navy-200 transition-colors hover:bg-navy-600",
+      )}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 };
 
--- frontend/pages/footprints/index.tsx
+++ frontend/pages/footprints/index.tsx
@@ -1,25 +1,39 @@
+import { useState } from "react";
+import { BottomSheet } from "react-spring-bottom-sheet";
+import shallow from "zustand/shallow";
+import RectangleButton from "../../components/buttons/RectangleButton";
 import MonthDropdown from "../../components/calendar/MonthDropdown";
 import WeekCalendar from "../../components/calendar/WeekCalendar";
 import Container from "../../components/containers/Container";
 import NavigationBar from "../../components/navbar/NavigationBar";
 import { useCalendarStore } from "../../store/calendar";
-import { getWeekContainingDate } from "../../utils/calendar";
-import shallow from "zustand/shallow";
-import RectangleButton from "../../components/buttons/RectangleButton";
+
+import "react-spring-bottom-sheet/dist/style.css";
+import FloatingButton from "../../components/buttons/FloatingButton";
+import { IconPlus } from "@tabler/icons";
 
 export default function Footprints() {
+  const [open, setOpen] = useState(false);
   const { selectedDate, selectedWeek, setSelectedDate } = useCalendarStore((state) => state, shallow);
   return (
     <Container>
-      <div className="sticky top-0 left-0 right-0 bg-navy-800/90 pb-4 backdrop-blur-md">
+      <div className="sticky top-0 left-0 right-0 border-b border-navy-700/50 bg-navy-800/90 pb-4 backdrop-blur-md">
         <NavigationBar />
         <MonthDropdown selectedDate={selectedDate} />
         <WeekCalendar week={selectedWeek} setSelectedDate={setSelectedDate} selectedDate={selectedDate} showWeekday />
       </div>
+      <div className="flex justify-center">
+        <FloatingButton
+          icon={IconPlus}
+          text="기록 추가하기"
+          onClick={() => {
+            setOpen(true);
+          }}
+          className="fixed bottom-0 my-5 text-right"
+        />
+      </div>
 
-      <RectangleButton text="기록 추가하기" onClick={() => {}} isLoading={false} className="my-5 shadow-lg" />
-
-      <div className="p-4 text-navy-200 ">{"fw e fa".repeat(3000)}</div>
+      <div className="p-4 text-navy-200">{"fw e fa".repeat(30)}</div>
     </Container>
   );
 }
