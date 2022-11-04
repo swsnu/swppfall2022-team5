4ce8604afd287a1efd09e6f8a359f83abd5d245b
lees741 <lees741@snu.ac.kr>
2022-11-04 17:36:39 +0900

    implement Footprint API

---
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/footprint/repository/FootprintRepository.kt
@@ -0,0 +1,7 @@
+package com.swpp.footprinter.domain.footprint.repository
+
+import com.swpp.footprinter.domain.footprint.model.Footprint
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface FootprintRepository : JpaRepository<Footprint, Long> {
+}
