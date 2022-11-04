a02ac7a8c9fb514f7640f457445c75d52184eab5
Park Shin Hong <shp7724@naver.com>
2022-11-04 18:44:56 +0900

    add disabled status

--- frontend/components/buttons/RectangleButton.tsx
+++ frontend/components/buttons/RectangleButton.tsx
@@ -5,18 +5,21 @@ interface IProps {
   text: string;
   isLoading: boolean;
   className?: string;
+  disabled?: boolean;
 }
 
-const RectangleButton = ({ onClick, text, isLoading, className }: IProps) => {
+const RectangleButton = ({ onClick, text, isLoading, className, disabled }: IProps) => {
   return (
     <button
       className={classNames(
         className,
-        "rounded-lg border border-navy-200/5 bg-navy-700 p-3 text-navy-200 transition-colors hover:bg-navy-600",
+        "rounded-lg border border-navy-200/5 bg-navy-700 p-3 text-navy-200 transition-colors ",
+        { "hover:bg-navy-600": !disabled },
       )}
       onClick={onClick}
+      disabled={disabled}
     >
-      {text}
+      <span className={classNames({ "opacity-20": disabled })}>{text}</span>
     </button>
   );
 };
