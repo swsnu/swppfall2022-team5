2768da123c1c6d95b5ef630a99aa191bbc91636c
lees741 <lees741@snu.ac.kr>
2022-11-04 16:59:11 +0900

    disable csrf for debugging

---
+++ backend/src/main/kotlin/com/swpp/footprinter/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.swpp.footprinter.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig(
+) {
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain =
+        httpSecurity
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .build()
+}
