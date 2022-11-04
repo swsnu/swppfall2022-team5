0133496538440fb43f4aa86a0612c26960c7d426
huGgW <whjoon0225@naver.com>
2022-11-04 19:15:58 +0900

    Added Place component to footprint (Kakaomap using react-kakao-maps-sdk)

--- frontend/components/footprint/Footprint.tsx
+++ frontend/components/footprint/Footprint.tsx
@@ -1,3 +1,4 @@
+import { Place, PlaceType } from "./Place";
 
 export interface FootprintType {
     id: number,
@@ -24,25 +25,26 @@ export interface MemoType {
     content: string,
 }
 
-export interface PlaceType {
-    id: number,
-    city: string,
-    country: string,
-    district: string,
-}
 
 export interface TagType {
     id: number,
     name: string,
 }
 
-export function Footprint(props: any) {
+let testPlace: PlaceType = {
+    id: 1, name: "TestName", city: "TestCity", country: "TestCountry",
+    district: "TestDistrict", latitude: 37.3306, longitude: 126.5930
+}
+
+export function Footprint(props: {place: PlaceType}) {
     return (
-        <div className="mx-3 mb-3">
-            <Place />
+        <div className="mx-3 mb-3 w-auto h-auto">
+            {/* <Place {...props.place} /> */}
+            <Place {...testPlace} />
+            {/* <Place />
             <TagList />
             <PhotoList />
-            <Memo />
+            <Memo /> */}
         </div>
     );
 }
---
+++ frontend/components/footprint/Place.tsx
@@ -0,0 +1,26 @@
+import Script from "next/script";
+import { Map, MapMarker, useInjectKakaoMapApi } from "react-kakao-maps-sdk"
+
+export interface PlaceType {
+    id: number,
+    name: string,
+    city: string,
+    country: string,
+    district: string,
+    longitude: number,
+    latitude: number,
+}
+
+export function Place(props: PlaceType) {
+    return (
+        <div className="mb-1 mx-1 rounded">
+            <h2 className="text-left"> {props.name} </h2>
+            <h3 className="text-left"> {props.city + "," + props.country + "," + props.district} </h3>
+            <Map center={{lat: props.latitude, lng: props.longitude}}
+                className="mt-1 w-full h-40 rounded"
+                level={10}
+            >
+            </Map>
+        </div>
+    );
+}
