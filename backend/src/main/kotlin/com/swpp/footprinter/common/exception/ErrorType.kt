47f3e7a1b4cf52c21b30719e7f917e5787df3207
huGgW <whjoon0225@naver.com>
2022-11-05 18:24:35 +0900

    Added 404 Error Type

--- backend/src/main/kotlin/com/swpp/footprinter/common/exception/ErrorType.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/common/exception/ErrorType.kt
@@ -8,6 +8,7 @@ enum class ErrorType(
     val message: String
 ) {
     MISSING_REQUEST_BODY(4001, HttpStatus.BAD_REQUEST, "요청 데이터가 존재하지 않습니다."),
+    NOT_FOUND(4004, HttpStatus.NOT_FOUND, "서버상에 데이터가 존재하지 않습니다."),
     SERVER_ERROR(5001, HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."),
     S3_UPLOAD_ERROR(5002, HttpStatus.INTERNAL_SERVER_ERROR, "미디어 업로드가 실패했습니다."),
 }
