6e362763a99591e8ce0ed860cba8ed00c3d54dfc
lees741 <lees741@snu.ac.kr>
2022-11-06 00:31:07 +0900

    implement coordinate to place API

--- backend/src/main/kotlin/com/swpp/footprinter/domain/place/service/PlaceService.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/place/service/PlaceService.kt
@@ -3,12 +3,9 @@ package com.swpp.footprinter.domain.place.service
 import com.swpp.footprinter.domain.place.repository.PlaceRepository
 import org.springframework.stereotype.Service
 
-interface PlaceService {
-}
+interface PlaceService
 
 @Service
-class PlaceServiceImpl (
+class PlaceServiceImpl(
     private val placeRepository: PlaceRepository
-) : PlaceService {
-
-}
+) : PlaceService
--- backend/src/main/kotlin/com/swpp/footprinter/domain/place/service/externalAPI/KakaoAPIService.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/place/service/externalAPI/KakaoAPIService.kt
@@ -10,19 +10,31 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 
 @Service
-class KakaoAPIService (
+class KakaoAPIService(
     @Value("\${kakao_apikey}")
     private val kakao_apikey: String
 ) {
-    private val url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"
-
     fun coordToRegion(longitude: String, latitude: String): ResponseEntity<String> {
+        val url = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json"
         val restTemplate = RestTemplate()
         val httpHeaders = HttpHeaders()
         httpHeaders.set("Authorization", "KakaoAK $kakao_apikey")
 
         val request = HttpEntity<String>(httpHeaders)
         val response: ResponseEntity<String> = restTemplate.exchange("$url?x=$longitude&y=$latitude", HttpMethod.GET, request, String::class)
+
+        return response
+    }
+
+    // Category code: MT1=대형마트 / CS2=편의점 / PS3=어린이집, 유치원 / SC4=학교 / AC5=학원 / PK6=주차장 / OL7=주유소, 충전소 / SW8=지하철역 / BK9=은행 / CT1=문화시설 / AG2=중개업소 / PO3=공공기관 / AT4=관광명소 / AD5=숙박 / FD6=음식점 / CE7=카페 / HP8=병원 / PM9=약국
+    fun coordToPlace(longitude: String, latitude: String, category: String, radius: Int): ResponseEntity<String> {
+        val url = "https://dapi.kakao.com/v2/local/search/category.json"
+        val restTemplate = RestTemplate()
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set("Authorization", "KakaoAK $kakao_apikey")
+
+        val request = HttpEntity<String>(httpHeaders)
+        val response: ResponseEntity<String> = restTemplate.exchange("$url?category_group_code=$category&x=$longitude&y=$latitude&radius=$radius&sort=distance", HttpMethod.GET, request, String::class)
 
         return response
     }
