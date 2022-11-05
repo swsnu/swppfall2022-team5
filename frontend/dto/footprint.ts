64bdd175d6cf48ec0700799d9b1a5e26023aa5b6
Park Shin Hong <shp7724@naver.com>
2022-11-06 00:14:05 +0900

    add predictions dto

--- frontend/dto/footprint.ts
+++ frontend/dto/footprint.ts
@@ -7,8 +7,19 @@ export interface FootprintType {
   startTime: string;
   endTime: string;
   rating: number;
-  photos: Array<PhotoType>;
+  photos: PhotoType[];
   place: PlaceType;
   tag: TagType;
   memo: string;
+}
+
+export interface FootprintPredictionsType {
+  predictions: FootprintPredictionType[];
+}
+
+export interface FootprintPredictionType {
+  startTime: string; // ISO8601 format, photos 중 가장 빠른 시각
+  endTime: string; // ISO8601 format, photos 중 가장 늦은 시각
+  predictedPlaces: PlaceType[];
+  photos: PhotoType[]; // 좌표 기준으로 그룹화 (오차범위는 50m 정도?)
 }
