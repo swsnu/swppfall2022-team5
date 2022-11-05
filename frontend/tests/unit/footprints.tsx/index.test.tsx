55770d1cc1e2c3443bfda32a86e86b0a99f857a3
Park Shin Hong <shp7724@naver.com>
2022-11-06 01:35:44 +0900

    add sample test

---
+++ frontend/tests/unit/footprints.tsx/index.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { useRouter } from "next/router";
+import Footprints from "../../../pages/footprints";
+
+describe("Home", () => {
+  it("redirect to /footprints", () => {
+    render(<Footprints />);
+    expect(screen).toBeTruthy();
+  });
+});
