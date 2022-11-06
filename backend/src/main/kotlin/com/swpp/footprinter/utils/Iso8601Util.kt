7ddffb3620071a999c1ee1866d771905471759a7
huGgW <whjoon0225@naver.com>
2022-11-06 17:36:57 +0900

    Change format & parsing implementation by using ISO_INSTANT

--- backend/src/main/kotlin/com/swpp/footprinter/utils/Iso8601Util.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/utils/Iso8601Util.kt
@@ -1,9 +1,19 @@
 package com.swpp.footprinter.utils
 
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAccessor
 import java.util.*
 
-val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.KOREAN)
-
-fun dateToString8601(date: Date): String = format.format(date)
-fun stringToDate8601(string: String): Date = format.parse(string)
+fun dateToString8601(date: Date): String {
+    val instant = date.toInstant()
+    val string = DateTimeFormatter.ISO_INSTANT.format(instant)
+    return string
+}
+fun stringToDate8601(string: String): Date {
+    val ta: TemporalAccessor = DateTimeFormatter.ISO_INSTANT.parse(string)
+    val i: Instant = Instant.from(ta)
+    val d = Date.from(i)
+    return d
+}
