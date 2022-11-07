ed8cbc979d16534ba3621d24a4177100a8ba639f
huGgW <whjoon0225@naver.com>
2022-11-07 11:17:48 +0900

    Added throwing exception when parsing went wrong

--- backend/src/main/kotlin/com/swpp/footprinter/common/utils/Iso8601Util.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/common/utils/Iso8601Util.kt
@@ -1,8 +1,12 @@
 package com.swpp.footprinter.common.utils
 
+import com.swpp.footprinter.common.exception.ErrorType
+import com.swpp.footprinter.common.exception.FootprinterException
 import java.text.SimpleDateFormat
+import java.time.DateTimeException
 import java.time.Instant
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 import java.time.temporal.TemporalAccessor
 import java.util.*
 
@@ -12,8 +16,12 @@ fun dateToString8601(date: Date): String {
     return string
 }
 fun stringToDate8601(string: String): Date {
-    val ta: TemporalAccessor = DateTimeFormatter.ISO_INSTANT.parse(string)
-    val i: Instant = Instant.from(ta)
-    val d = Date.from(i)
-    return d
+    try {
+        val ta: TemporalAccessor = DateTimeFormatter.ISO_INSTANT.parse(string)
+        val i: Instant = Instant.from(ta)
+        val d = Date.from(i)
+        return d
+    } catch (e: DateTimeParseException) {
+        throw FootprinterException(ErrorType.WRONG_FORMAT)
+    }
 }
