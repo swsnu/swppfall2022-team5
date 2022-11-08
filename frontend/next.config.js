30380875c03fa50a3eef85a6b08ffdff3723ec0d
huGgW <whjoon0225@naver.com>
2022-11-08 14:11:54 +0900

    Added config for connecting to backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ["images.unsplash.com"] },
+
+  // Proxy for solving CORS in frontend-backend
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*'
+      }
+    ]
+  }
 };
 
 module.exports = withPWA(nextConfig);
