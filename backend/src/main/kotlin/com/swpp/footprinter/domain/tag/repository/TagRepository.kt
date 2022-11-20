3f029f59f56cd3ceb882736aec005346fc0ac4de
Park Shin Hong <shp7724@naver.com>
2022-11-20 20:24:48 +0900

    add @Repository annotation

--- backend/src/main/kotlin/com/swpp/footprinter/domain/tag/repository/TagRepository.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/tag/repository/TagRepository.kt
@@ -3,7 +3,9 @@ package com.swpp.footprinter.domain.tag.repository
 import com.swpp.footprinter.domain.tag.TAG_CODE
 import com.swpp.footprinter.domain.tag.model.Tag
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TagRepository : JpaRepository<Tag, Long> {
     fun findByTagCode(code: TAG_CODE): Tag?
     fun existsByTagCode(code: TAG_CODE): Boolean
