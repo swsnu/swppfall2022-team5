2c5dbcb29dac947e61e204bfa26a3a2137957750
Park Shin Hong <shp7724@naver.com>
2022-11-20 20:22:41 +0900

    configure password encoder bean

--- backend/src/main/kotlin/com/swpp/footprinter/config/SecurityConfig.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/config/SecurityConfig.kt
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
@@ -17,4 +19,7 @@ class SecurityConfig {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .build()
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
