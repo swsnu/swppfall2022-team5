40a5e79a90c2ef4fbb19af3634e3e65d6ad37359
lees741 <lees741@snu.ac.kr>
2022-11-22 19:38:54 +0900

    implement draft trace pages

---
+++ frontend/components/placeholder/TracesNotFound.tsx
@@ -0,0 +1,14 @@
+import { IconCalendarMinus } from "@tabler/icons";
+
+const TracesNotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center pt-32 text-navy-700">
+      <IconCalendarMinus size={150} stroke={2} />
+      <div className="mt-5 text-center text-navy-500/50">
+        <div className="mt-3 text-center text-xl font-bold">Trace가 존재하지 않습니다</div>
+      </div>
+    </div>
+  );
+};
+
+export default TracesNotFound;
--- frontend/components/trace/TracePreview.tsx
+++ frontend/components/trace/TracePreview.tsx
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
 import { TraceDetailResponse } from "../../dto/trace";
-import Photo from "../footprint/Photo";
+import PhotoPreview from "../trace/PhotoPreview";
 
 interface IProps extends TraceDetailResponse {}
 
 export function TracePreview(props: IProps) {
     const router = useRouter();
-    console.log(props.footprints[0].photos)
+    
     return (
         <div
             className="p-5 text-navy-100 transition-colors hover:cursor-pointer hover:bg-navy-800/50"
@@ -18,7 +18,7 @@ export function TracePreview(props: IProps) {
                 <div className="mt-2">{props.title}</div>
                 <div className="mt-1 text-xs text-navy-500">{props.date}</div>
             </div>
-            <Photo key={props.footprints[0].photos[0].id} {...props.footprints[0].photos[0]} />
+            <PhotoPreview key={props.footprints[0].photos[0].id} {...props.footprints[0].photos[0]} />
             
         </div>
     )
