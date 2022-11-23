2304cf1b08da031358f65c16a0ce59086ce9dd4b
JuTaK97 <didwngus01@snu.ac.kr>
2022-11-24 05:28:46 +0900

    로그인 화면 이미지 임시

--- frontend/pages/signin/index.tsx
+++ frontend/pages/signin/index.tsx
@@ -10,7 +10,7 @@ import { useAuthStore } from '../../store/auth';
 import { apiClient } from "../../api/client";
 import { ErrorType } from "../../api/exception/errorType";
 import Image from "next/image";
-
+import image from "../../public/img/tutorial.jpeg"
 
 export default function Signin() {
     const [inputId, setInputId] = useState("")
@@ -24,7 +24,7 @@ export default function Signin() {
     return (
         <Container>
             <div className='mx-5'>
-                <div className="pt-20 text-2xl font-bold">
+                <div className="pt-10 text-2xl font-bold">
                     당신의 발자취를 이곳에 기록하세요
                 </div>
                 <div className="mt-2 text-xs text-gray-400">
@@ -33,39 +33,31 @@ export default function Signin() {
                 <div className="text-xs text-gray-400">
                     사진을 한번에 업로드하세요. 자동으로 분류하여 정리해 드립니다.
                 </div>
+                <div className="grid justify-items-center">
+                    <Image
+                        className="mt-5 rounded-3xl"
+                        src={image}
+                        alt="Sample Image"
+                        width={300}>
+                    </Image>
+                </div>
+                
 
-                <Image
-                    className="mt-10 flex justify-center"
-                    src=""
-                    alt="Sample Image"
-                    width={500}
-                    height={300}
-                >
-                </Image>
-
-                <div className="mt-20 grid grid-cols-1 place-items-center">
-                    {/* <label className="text-white-900 text-sm font-bold p-2">
-                        아이디
-                    </label> */}
+                <div className="mt-5 grid grid-cols-1 place-items-center">
                     <input 
-                        className="text-white-900 bg-black w-3/5 p-3 enabled:hover:border-white-400"
+                        className="text-white-900 text-sm bg-black w-3/5 p-2 enabled:hover:border-white-400 rounded-xl"
                         placeholder='아이디를 입력하세요.'
                         onChange={event => setInputId(event.currentTarget.value)}
                     />
-                
-                
-                    {/* <label className="block text-white-900 text-sm font-bold mt-3 p-2">
-                        비밀번호
-                    </label> */}
                     <input 
-                        className="text-white-900 bg-black w-3/5 p-3 enabled:hover:border-white-400 mt-2"
+                        className="text-white-900 text-sm bg-black w-3/5 p-2 enabled:hover:border-white-400 rounded-xl mt-2"
                         placeholder='비밀번호를 입력하세요.'
                         type='password'
                         onChange={event => setInputPassword(event.currentTarget.value)}
                     />
                 </div>
 
-                <div className='flex justify-center mt-5'>
+                <div className='flex justify-center mt-3'>
                     <RectangleButton
                         onClick={() => {
                             mutation.mutate(
@@ -94,12 +86,12 @@ export default function Signin() {
                         }}
                         text={"로그인"}
                         isLoading={false}
-                        className='w-3/5 p-2'
+                        className='w-3/5 p-1 rounded-2xl'
                         disabled={false}
                     />
                 </div>
 
-                <div className='flex justify-center mt-3'>
+                <div className='flex justify-center mt-2'>
                     <RectangleButton
                         onClick={() => {
                             signupMutation.mutate(
@@ -128,7 +120,7 @@ export default function Signin() {
                         }}
                         text={"회원가입"}
                         isLoading={false}
-                        className='w-3/5 p-2'
+                        className='w-3/5 p-1 rounded-2xl'
                         disabled={false}
                     />
                 </div>
