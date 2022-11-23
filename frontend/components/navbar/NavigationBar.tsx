d817d2fe9eb5bf4a833854fe07f60c7cc8370b22
JuTaK97 <didwngus01@snu.ac.kr>
2022-11-24 02:15:37 +0900

    유저 아이콘 드롭다운 및 로그아웃 로직

--- frontend/components/navbar/NavigationBar.tsx
+++ frontend/components/navbar/NavigationBar.tsx
@@ -1,16 +1,28 @@
 import { IconMenu2, IconUser } from "@tabler/icons";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import { useAuthStore } from "../../store/auth";
 import IconButton from "../buttons/IconButton";
+import UserDropdown from "../user/UserDropdown";
 
 interface IProps {
   title?: string;
 }
 
 const NavigationBar = ({ title }: IProps) => {
+  const router = useRouter()
+  const setToken = useAuthStore.getState().setToken
   return (
     <div className="flex items-center justify-between px-3 py-2">
       <IconButton icon={IconMenu2} onClick={() => {}} />
       <div>{title}</div>
-      <IconButton icon={IconUser} onClick={() => {}} />
+      <UserDropdown
+        onClickSignout={() => {
+          setToken("")
+          router.push("/signin")
+          toast("로그아웃 되었습니다.")
+        }}
+      />
     </div>
   );
 };
