c6d405e52e4b688ac6fbaff571cddea924363216
huGgW <whjoon0225@naver.com>
2022-11-24 04:12:10 +0900

    Added feature to search near places by keyword

--- backend/src/main/kotlin/com/swpp/footprinter/domain/place/dto/PlaceResponse.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/place/dto/PlaceResponse.kt
@@ -3,4 +3,6 @@ package com.swpp.footprinter.domain.place.dto
 data class PlaceResponse(
     val name: String,
     val address: String,
+    val distance: Double ? = null,
+    val tagId: Int? = null,
 )
--- backend/src/main/kotlin/com/swpp/footprinter/domain/place/service/PlaceService.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/place/service/PlaceService.kt
@@ -1,11 +1,52 @@
 package com.swpp.footprinter.domain.place.service
 
+import com.swpp.footprinter.domain.place.dto.PlaceResponse
+import com.swpp.footprinter.domain.place.dto.PlaceSearchRequest
 import com.swpp.footprinter.domain.place.repository.PlaceRepository
+import com.swpp.footprinter.domain.place.service.externalAPI.KakaoAPIService
 import org.springframework.stereotype.Service
+import com.swpp.footprinter.common.PLACE_SEARCH_METER
+import com.swpp.footprinter.common.exception.ErrorType
+import com.swpp.footprinter.common.exception.FootprinterException
+import com.swpp.footprinter.domain.tag.TAG_CODE
 
-interface PlaceService
+interface PlaceService {
+    fun searchPlacesByKeywordAndTags(placeSearchRequest: PlaceSearchRequest): List<PlaceResponse>
+}
 
 @Service
 class PlaceServiceImpl(
-    private val placeRepository: PlaceRepository
-) : PlaceService
+    private val placeRepository: PlaceRepository,
+    private val kakaoAPIService: KakaoAPIService,
+) : PlaceService {
+    override fun searchPlacesByKeywordAndTags(placeSearchRequest: PlaceSearchRequest): List<PlaceResponse> {
+        val searchedPlacesAllTagsResponse = placeSearchRequest.let {
+            kakaoAPIService.coordAndKeywordToPlace(
+                it.keyword, it.longitude!!, it.latitude!!, PLACE_SEARCH_METER
+            )
+        }
+
+        val searchedPlacesAllTagsList = kakaoAPIService.getDocumentsMapListFromResponse(searchedPlacesAllTagsResponse)
+
+        val searchTagCodeValueList = placeSearchRequest.tagIDs.map {
+            try {
+                TAG_CODE.values()[it].code
+            } catch (e: IndexOutOfBoundsException) { // For wrong tag id given
+                throw FootprinterException(ErrorType.WRONG_FORMAT)
+            }
+        }
+
+        val searchedPlacesWithTagsList = searchedPlacesAllTagsList.filter {
+            searchTagCodeValueList.contains(it["category_group_code"])
+        }
+
+        return searchedPlacesWithTagsList.map {
+            PlaceResponse(
+                name = it["place_name"]!!,
+                address = it["address_name"]!!,
+                distance = it["distance"]!!.toDouble(),
+                tagId = TAG_CODE.values().find { t -> t.code == it["category_group_code"] }!!.ordinal
+            )
+        }
+    }
+}
