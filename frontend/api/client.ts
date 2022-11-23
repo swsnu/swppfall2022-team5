d85b25d122847dbb3052dc7236282913d546e56f
JuTaK97 <didwngus01@snu.ac.kr>
2022-11-23 13:38:55 +0900

    apiClient를 쓸 때마다 store에서 token 가져오도록 변경

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -1,3 +1,14 @@
 import axios from "axios";
+import { useAuthStore } from "../store/auth";
 
-export const apiClient = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+export const apiClient = () =>  {
+    const token = useAuthStore.getState().userToken
+    return axios.create(
+        { 
+            baseURL: process.env.NEXT_PUBLIC_API_URL,
+            headers: {
+                'Authorization' : `Bearer ${token}`
+            },
+        }
+    )
+};
--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -5,41 +5,39 @@ import moment from "moment";
 import { TagType } from "../dto/tag";
 import { FootprintEditRequestType, FootprintRequestType, FootprintResponseType } from "../dto/footprint";
 import { SigninRequestType, SigninResponseType } from "../dto/auth";
-import { useAuthStore } from "../store/auth";
 
 export const fetchInitialFootprints = async (photoIds: string[]) => {
-  return (await apiClient.post<FootprintPredictionType[]>("/traces/create", photoIds)).data;
+  return (await apiClient().post<FootprintPredictionType[]>("/traces/create", photoIds)).data;
 };
 
 export const createTrace = async (traceRequest: TraceRequestType) => {
-  return (await apiClient.post<String>("/traces", traceRequest)).data;
+  return (await apiClient().post<String>("/traces", traceRequest)).data;
 };
 
 export const editFootprint = async (footprintId: number, footprintRequest: FootprintEditRequestType) => {
-  return (await apiClient.put<String>(`/footprints/${footprintId}`, footprintRequest)).data;
+  return (await apiClient().put<String>(`/footprints/${footprintId}`, footprintRequest)).data;
 };
 
 export const fetchTraceByDate = async (date: Date) => {
-  const token = useAuthStore.getState().userToken
-  return (await apiClient.get<TraceDetailResponse>(`/traces/date/${moment(date).format("YYYY-MM-DD")}`, { headers: {'Authorization': `Bearer ${token}`} })).data;
+  return (await apiClient().get<TraceDetailResponse>(`/traces/date/${moment(date).format("YYYY-MM-DD")}`)).data;
 };
 
 export const fetchTraceById = async (traceId: number) => {
-  return await apiClient.get<TraceDetailResponse>(`/traces/id/${traceId}`);
+  return await apiClient().get<TraceDetailResponse>(`/traces/id/${traceId}`);
 };
 
 export const fetchTags = async () => {
-  return (await apiClient.get<TagType[]>(`/tags`)).data;
+  return (await apiClient().get<TagType[]>(`/tags`)).data;
 };
 
 export const fetchFootprintById = async (footprintId: number) => {
-  return (await apiClient.get<FootprintResponseType>(`/footprints/${footprintId}`)).data;
+  return (await apiClient().get<FootprintResponseType>(`/footprints/${footprintId}`)).data;
 };
 
 export const postSignin = async (signinRequest: SigninRequestType) => {
-  return (await apiClient.post<SigninResponseType>("/signin", signinRequest)).data;
+  return (await apiClient().post<SigninResponseType>("/signin", signinRequest)).data;
 };
 
 export const postSignUp = async (signinRequest: SigninRequestType) => {
-  return (await apiClient.post<SigninResponseType>("/signup", signinRequest)).data;
+  return (await apiClient().post<SigninResponseType>("/signup", signinRequest)).data;
 };
