65bc0e8e2c6e2421076c0fb08ae902dab0708978
lees741 <lees741@snu.ac.kr>
2022-11-25 20:38:22 +0900

    fix linter issue

--- backend/src/main/kotlin/com/swpp/footprinter/domain/user/api/UserController.kt
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/user/api/UserController.kt
@@ -1,12 +1,8 @@
 package com.swpp.footprinter.domain.user.api
 
-import com.swpp.footprinter.domain.user.service.UserService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/user")
-class UserController(
-    private val service: UserService
-) {
-}
+class UserController()
