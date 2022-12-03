95ed706add95f7e085e0f9c8278a0ac1434ab6f0
huGgW <whjoon0225@naver.com>
2022-12-04 01:37:05 +0900

    Added DTO for searching trace request

---
+++ backend/src/main/kotlin/com/swpp/footprinter/domain/trace/dto/TraceSearchRequest.kt
@@ -0,0 +1,11 @@
+package com.swpp.footprinter.domain.trace.dto
+
+import com.swpp.footprinter.domain.place.dto.PlaceRequest
+import com.swpp.footprinter.domain.user.dto.UserRequest
+
+data class TraceSearchRequest(
+    val users: List<UserRequest>,
+    val tags: List<Int>,
+    val dates: List<String>,
+    val places: List<PlaceRequest>,
+)
