a6e03327e427ad7c446ed2a5dcd7c79c567c63f6
lees741 <lees741@snu.ac.kr>
2022-12-05 11:56:35 +0900

    add like & view count

--- frontend/components/trace/OwnerInfo.tsx
+++ frontend/components/trace/OwnerInfo.tsx
@@ -4,7 +4,7 @@ import OwnerProfilePhoto from "./OwnerProfilePhoto";
 const OwnerInfo = (profile: { username: string }) => {
     const router = useRouter();
     return (
-        <div className="flex">
+        <div className="flex pt-3">
             <OwnerProfilePhoto imageUrl="https://images.unsplash.com/photo-1669072596436-15df4a8c083d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80" />
             <div className="text-base p-3 text-center hover:cursor-pointer" onClick={() => router.push(`/user/${profile.username}`)}>
                 {profile.username}
