4a5b591fbb915889762f61288465b31434751df7
이현성 <40167175+lees741@users.noreply.github.com>
2022-12-10 16:51:02 +0900

    ✅ Add frontend api test (#78)
    * fix typo
    
    * add mock api test

--- frontend/tests/unit/api/index.test.ts
+++ frontend/tests/unit/api/index.test.ts
@@ -1,5 +1,5 @@
 import { act } from "@testing-library/react";
-import { editFootprint, fetchAllOtherUsersTraces, fetchAllUserTraces, fetchFootprintById, fetchInitialFootprints, fetchTraceById, postSignin, postSignUp } from "../../../api";
+import { deleteFootprint, editFootprint, fetchAllOtherUsersTraces, fetchAllUserTraces, fetchFootprintById, fetchInitialFootprints, fetchIsFollowing, fetchMe, fetchRegionByCoordinates, fetchTraceById, fetchUserByUsername, followUser, likeTrace, postSignin, postSignUp, unfollowUser, unlikeTrace, updateViewCount } from "../../../api";
 import { SigninRequestType } from "../../../dto/auth";
 import { FootprintEditRequestType, FootprintRequestType } from "../../../dto/footprint";
 import mockAxios from "../../../__mocks__/axios";
@@ -82,6 +82,86 @@ it("post postSignUp", async () => {
   mockAxios.post.mockResolvedValue({ data: "mock" });
   await act(async () => {
     const data = await postSignUp(authRequestStub);
+    expect(data).toBe("mock");
+  });
+});
+
+it("like trace", async () => {
+  mockAxios.post.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await likeTrace(1);
+    expect(data).toBe("mock");
+  });
+});
+
+it("unlike trace", async () => {
+  mockAxios.delete.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await unlikeTrace(1);
+    expect(data).toBe("mock");
+  });
+});
+
+it("fetch region by coordinates", async () => {
+  mockAxios.get.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await fetchRegionByCoordinates(1, 1);
+    expect(data).toBe("mock");
+  });
+});
+
+it("update view count", async () => {
+  mockAxios.post.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await updateViewCount(1);
+    expect(data).toBe("mock");
+  });
+});
+
+it("fetch user by username", async () => {
+  mockAxios.get.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await fetchUserByUsername("user");
+    expect(data).toBe("mock");
+  });
+});
+
+it("fetch me", async () => {
+  mockAxios.get.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await fetchMe();
+    expect(data).toBe("mock");
+  });
+});
+
+it("fetch following", async () => {
+  mockAxios.get.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await fetchIsFollowing("user");
+    expect(data).toBe("mock");
+  });
+});
+
+it("follow user", async () => {
+  mockAxios.post.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await followUser("user");
+    expect(data).toBe("mock");
+  });
+});
+
+it("unfollow user", async () => {
+  mockAxios.delete.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await unfollowUser("user");
+    expect(data).toBe("mock");
+  });
+});
+
+it("delete footprint", async () => {
+  mockAxios.delete.mockResolvedValue({ data: "mock" });
+  await act(async () => {
+    const data = await deleteFootprint(1);
     expect(data).toBe("mock");
   });
 });
